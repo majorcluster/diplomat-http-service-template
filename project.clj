0627cc8f1d7ec134a376edaac32d393106869f91
Mateus Carvalho <mcarvalho@MATEUSCMAC.mgm-edv.de>
2022-05-31 13:20:56 +0200

    Added infos to project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.majorcluster/lein-template.diplomat-http-service "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Diplomat architecture-pedestal styled template for leiningen generation"
+  :url "https://github.com/mtsbarbosa/diplomat-http-service-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
