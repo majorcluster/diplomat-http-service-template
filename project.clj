86c19f26ae6ba1e526ece484a7e7e0e9be3cdcec
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-03-18 15:52:36 -0300

    Adjusting license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.majorcluster/lein-template.diplomat-http-service "0.3.0"
+(defproject org.clojars.majorcluster/lein-template.diplomat-http-service "0.3.1"
   :description "Diplomat architecture-pedestal styled template for leiningen generation"
   :url "https://github.com/mtsbarbosa/diplomat-http-service-template"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :eval-in-leiningen true)
--- resources/leiningen/new/diplomat_http_service/project.clj
+++ resources/leiningen/new/diplomat_http_service/project.clj
@@ -1,8 +1,8 @@
 (defproject {{raw-name}} "0.0.1-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-            :license {:name "Eclipse Public License"
-                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :license {:name "The MIT License"
+                      :url "http://opensource.org/licenses/MIT"}
             :dependencies [[org.clojure/clojure "1.11.1"]
                            [io.pedestal/pedestal.service "0.5.10"]
                            [io.pedestal/pedestal.jetty "0.5.10"]
