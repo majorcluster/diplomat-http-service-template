cdf3da655462679b81755a5a85888ef3046077a6
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-03-18 15:53:21 -0300

    Adjusting github url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.majorcluster/lein-template.diplomat-http-service "0.3.1"
   :description "Diplomat architecture-pedestal styled template for leiningen generation"
-  :url "https://github.com/mtsbarbosa/diplomat-http-service-template"
+  :url "https://github.com/majorcluster/diplomat-http-service-template"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :eval-in-leiningen true)
