ff82918c7ccf0ef11baa28d26a3914b03717924d
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-04-06 23:40:23 -0300

    adding auto-deploy-script

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject org.clojars.majorcluster/lein-template.diplomat-http-service "0.3.3"
+(defproject org.clojars.majorcluster/lein-template.diplomat-http-service "0.3.4"
   :description "Diplomat architecture-pedestal styled template for leiningen generation"
   :url "https://github.com/majorcluster/diplomat-http-service-template"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
   :eval-in-leiningen true)
